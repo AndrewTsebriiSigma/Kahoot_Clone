d72e6cdc5aa47737aed518d2bee292b468af73eb
ImmyB91 <imran.bhaiyat.12302004@gmail.com>
2024-11-28 09:42:13 -0500

    Create DashBoard.jsx
    Added a component

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+  
+    return (
+      <div className="container">
+        <h1>Welcome to Pass Random Quiz!</h1>
+        <button onClick={() => navigate('/quiz-setup')}>Pass Random Quiz</button>
+      </div>
+    );
+};
+
+export default Dashboard;
