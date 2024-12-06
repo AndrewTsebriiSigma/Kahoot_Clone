a3a8e5cd00401d2d3b57a4a1b7b144b43926b73e
Harsheary <harsheary2022005@gmail.com>
2024-12-06 06:41:32 -0500

    update App.jsx
    routes for StudentQuiz and TeacherQuiz

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,31 @@ import PassRandomQuizDasboard from './components/PassRandomQuizDashboard.jsx';
 import PassRandomQuizSetup from './components/PassRandomQuizSetup.jsx';
 import PassRandomQuiz from './components/PassRandomQuiz.jsx';
 import PassRandomQuizResults from './components/PassRandomQuizResults.jsx';
+import StudentQuiz from './Components/StudentQuiz.jsx';
+import TeacherQuiz from './Components/TeacherQuiz.jsx';
 
-function App () {
+function App() {
   return (
     <>
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/quizzes" element={<Quizzes />} />
-          <Route path="/join-page" element={<JoinPage />} />
-          <Route path="/lobby" element={<Lobby/>}/> 
-          <Route path="/pass-random-quiz" element={<PassRandomQuizDasboard />} />
-          <Route path="/random-quiz-setup" element={<PassRandomQuizSetup/>} />
-          <Route path="/random-quiz" element={<PassRandomQuiz />} />
-          <Route path="/random-results" element={<PassRandomQuizResults />} />
-          <Route path="/create-quiz" element={<CreateQuiz />}/>
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/quizzes" element={<Quizzes />} />
+            <Route path="/join-page" element={<JoinPage />} />
+            <Route path="/lobby" element={<Lobby />} />
+            <Route path="/pass-random-quiz" element={<PassRandomQuizDasboard />} />
+            <Route path="/random-quiz-setup" element={<PassRandomQuizSetup />} />
+            <Route path="/random-quiz" element={<PassRandomQuiz />} />
+            <Route path="/random-results" element={<PassRandomQuizResults />} />
+            <Route path="/create-quiz" element={<CreateQuiz />} />
+            <Route path="/teacher-quiz/:quizCode" element={<TeacherQuiz />} />
+            <Route path="/student-quiz/:quizCode" element={<StudentQuiz />} />
+          </Routes>
+        </div>
+      </Router>
 
     </>
   );
